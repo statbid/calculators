1845ef100f242c23bb83763431e4bf50e6c46a4b
Sabeur Arfaoui <117772841+arfaouisabeur@users.noreply.github.com>
2024-10-08 09:39:11 +0100

    Marketing calculator final code

--- views/pages/app/lib/prototypes.js
+++
@@ -1,9 +0,0 @@
-'use strict'
-
-Number.prototype.formatMoney = function (e, t, u) {
-  var n = this,
-    r = (e = isNaN(e = Math.abs(e)) ? 2 : e, t = null == t ? "." : t, u = null == u ? "," : u, n < 0 ? "-" : ""),
-    a = parseInt(n = Math.abs(+n || 0).toFixed(e)) + "",
-    d = (d = a.length) > 3 ? d % 3 : 0;
-  return r + (d ? a.substr(0, d) + u : "") + a.substr(d).replace(/(\d{3})(?=\d)/g, "$1" + u) + (e ? t + Math.abs(n - a).toFixed(e).slice(2) : "");
-}
